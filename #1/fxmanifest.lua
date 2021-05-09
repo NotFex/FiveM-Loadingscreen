f1443c41d815217fd8ff5d0a8d1476a2b55b6ec3
Felix Burwitz <62483138+NotFex@users.noreply.github.com>
2021-05-09 03:24:51 +0200

    Add files via upload

---
+++ #1/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'NotFex'
+description 'LoadingScreen by NotFex.'
+version '4.2.0'
+
+files {
+    'index.html',
+    'css/style.css',
+    'js/loading.js',
+    'js/typewriter.js'
+    'js/app.js',
+    'js/particles.min.js',
+}
+
+loadscreen 'index.html'
